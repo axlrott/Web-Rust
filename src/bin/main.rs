8ee61874c88df676bb2f6c0730aed31c35e6e128
Axel Aparicio <axl_rott@hotmail.com>
2022-07-18 14:02:50 -0300

    Elimine unwraps en main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,13 +4,13 @@ use std::{
     net::{TcpListener, TcpStream},
     sync::{Arc, Mutex},
     thread,
-    time::Duration,
+    time::Duration, error::Error,
 };
 
 use web_rust::thread_pool::ThreadPool;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+fn main() -> Result<(), Box<dyn Error>>{
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
     let _ = listener.set_nonblocking(true);
     let pool = ThreadPool::new(4);
     let shutdown = Arc::new(Mutex::new(false));
@@ -20,34 +20,46 @@ fn main() {
     thread::spawn(move || loop {
         println!("Waiting for input");
         let mut command = String::new();
-        let _ = std::io::stdin().read_line(&mut command).unwrap();
+        let _ = std::io::stdin().read_line(&mut command);
         if command == exit_command {
             println!("Execute exit command");
-            *shutdown_copy.lock().unwrap() = true;
+            match shutdown_copy.lock() {
+                Ok(mut mutex) => *mutex = true,
+                _ => (), //Ver que hacer en casos de error
+            }
         }
     });
     loop {
+        println!("Listening...");
         match listener.accept() {
             Ok((stream, sock_addr)) => {
                 println!("Conected to {}", sock_addr);
                 pool.execute(|| {
-                    handle_connection(stream);
+                    match handle_connection(stream) {
+                        Ok(_) => (),
+                        Err(error) => println!("{}", error), //Ver que hacer es casos de error
+                    }
                 });
             }
             Err(error) => {
                 if error.kind() == ErrorKind::WouldBlock {
-                    if *shutdown.lock().unwrap() {
-                        break;
-                    } else {
-                        thread::sleep(Duration::from_secs(1));
+                    match shutdown.lock() {
+                        Ok(mutex) => {
+                            if *mutex {
+                                break;
+                            }
+                        },
+                        _ => (), //Ver que hacer en casos de error
                     }
+                    thread::sleep(Duration::from_secs(1));
                 }
             }
         };
     }
+    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>>{
     let mut buffer = [0; 1024];
     let _ = stream.read(&mut buffer);
 
@@ -58,7 +70,7 @@ fn handle_connection(mut stream: TcpStream) {
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename)?;
 
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
@@ -67,6 +79,7 @@ fn handle_connection(mut stream: TcpStream) {
         contents
     );
 
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes())?;
+    stream.flush()?;
+    Ok(())
 }
