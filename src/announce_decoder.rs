117fa910214eab07dd35c978b8322653d3baad0f
Axel Aparicio <axl_rott@hotmail.com>
2022-07-19 16:25:22 -0300

    Continuacion de la decodificacion del announce

--- src/announce_decoder.rs
+++ src/announce_decoder.rs
@@ -7,6 +7,8 @@ pub const UPLOADED: &str = "uploaded=";
 pub const LEFT: &str = "left=";
 pub const PORT: &str = "port=";
 pub const EVENT: &str = "event=";
+pub const IP: &str = "ip=";
+pub const COMPACT: &str = "compact=";
 
 fn find_index_msg(response: &[u8], size: usize, end_line: &[u8]) -> Option<usize> {
     let first_pos = response.windows(size).position(|arr| arr == end_line);
@@ -14,11 +16,17 @@ fn find_index_msg(response: &[u8], size: usize, end_line: &[u8]) -> Option<usize
 }
 
 pub struct Announce {
+    //OBLIGATORIOS
     info_hash: Vec<u8>,
     peer_id: Vec<u8>,
+    port: u64,
     downloaded: u64,
     uploaded: u64,
     left: u64,
+    //OPCIONALES
+    ip: Option<Vec<u8>>,
+    compact: Option<Vec<u8>>,
+    event: Option<Vec<u8>>,
 }
 
 fn take_result(announce: &[u8]) -> Vec<u8> {
@@ -32,47 +40,75 @@ fn take_result(announce: &[u8]) -> Vec<u8> {
     result
 }
 
-fn init_command_vec(announce: Vec<u8>, size_command: usize, command: &str) -> Vec<u8> {
-    let pos_result = find_index_msg(&announce, size_command, command.as_bytes());
-    match pos_result {
-        Some(pos) => take_result(&announce[pos..]),
-        None => vec![],
+fn init_command_vec(announce: &[u8], size_command: usize, command: &str) -> Option<Vec<u8>> {
+    let pos_result = find_index_msg(announce, size_command, command.as_bytes());
+    pos_result.map(|pos| take_result(&announce[pos..]))
+}
+
+fn init_command_u64(announce: &[u8], size_command: usize, command: &str) -> u64 {
+    let response_vector = init_command_vec(announce, size_command, command);
+    match response_vector {
+        Some(vector) => {
+            let str = String::from_utf8_lossy(&vector).to_string();
+            str.parse().unwrap_or(0)
+        }
+        None => 0,
     }
 }
 
-fn init_command_u64(announce: Vec<u8>, size_command: usize, command: &str) -> u64 {
-    let vector = init_command_vec(announce, size_command, command);
-    let str = String::from_utf8_lossy(&vector).to_string();
-    str.parse().unwrap_or(0)
-}
-
 impl Announce {
-    pub fn new(announce_str: Vec<u8>) -> Self {
-        let info_hash = init_command_vec(announce_str.clone(), INFO_HASH.len(), INFO_HASH);
-        let peer_id = init_command_vec(announce_str.clone(), PEER_ID.len(), PEER_ID);
-        let downloaded = init_command_u64(announce_str.clone(), DOWNLOADED.len(), DOWNLOADED);
-        let uploaded = init_command_u64(announce_str.clone(), UPLOADED.len(), UPLOADED);
-        let left = init_command_u64(announce_str, LEFT.len(), LEFT);
+    pub fn new(announce: Vec<u8>) -> Self {
+        let info_hash = init_command_vec(&announce, INFO_HASH.len(), INFO_HASH).unwrap();
+        let peer_id = init_command_vec(&announce, PEER_ID.len(), PEER_ID).unwrap();
+        let port = init_command_u64(&announce, PORT.len(), PORT);
+        let downloaded = init_command_u64(&announce, DOWNLOADED.len(), DOWNLOADED);
+        let uploaded = init_command_u64(&announce, UPLOADED.len(), UPLOADED);
+        let left = init_command_u64(&announce, LEFT.len(), LEFT);
+
+        let ip = init_command_vec(&announce, IP.len(), IP);
+        let compact = init_command_vec(&announce, COMPACT.len(), COMPACT);
+        let event = init_command_vec(&announce, EVENT.len(), EVENT);
 
         Announce {
             info_hash,
             peer_id,
+            port,
             downloaded,
             uploaded,
             left,
+            ip,
+            compact,
+            event,
         }
     }
 
     pub fn get_announce_str(&self) {
         let mut log_file = File::create("announce.html").unwrap();
 
+        let ip = match self.ip.clone() {
+            Some(vec) => vec,
+            None => b"None".to_vec(),
+        };
+        let compact = match self.compact.clone() {
+            Some(vec) => vec,
+            None => b"None".to_vec(),
+        };
+        let event = match self.event.clone() {
+            Some(vec) => vec,
+            None => b"None".to_vec(),
+        };
+
         let a = format!(
-            "INFO_HASH: {}\nPEER_ID: {}\nDOWNLOADED: {}\nUPLOADED: {}\nLEFT: {}\n",
+            "INFO_HASH: {}\nPEER_ID: {}\nPORT: {}\nDOWNLOADED: {}\nUPLOADED: {}\nLEFT: {}\nIP: {}\nCOMPACT: {}\nEVENT: {}\n",
             String::from_utf8_lossy(&self.info_hash),
             String::from_utf8_lossy(&self.peer_id),
+            self.port,
             self.downloaded,
             self.uploaded,
-            self.left
+            self.left,
+            String::from_utf8_lossy(&ip),
+            String::from_utf8_lossy(&compact),
+            String::from_utf8_lossy(&event),
         );
 
         let _ = log_file.write_all(a.as_bytes());
