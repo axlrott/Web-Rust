cb96dfff3f2f2f7bcd1ca8276a574ff960b5b0fa
Axel Aparicio <axl_rott@hotmail.com>
2022-08-12 19:44:50 -0300

    Agregue tambien el grafico de barras

--- js/code.js
+++ js/code.js
@@ -2,7 +2,7 @@ var xValues = ['2022-01-01', '2022-02-01', '2022-03-01', '2022-04-01', '2022-05-
 '2022-08-01', '2022-09-01', '2022-10-01', '2022-11-01', '2022-12-01'];
 var yValues = [2, 8, 10, 12, 15, 20, 22, 25, 29, 33, 40, 42];
 
-new Chart("myChart", {
+new Chart("lineChart", {
   type: "line",
   data: {
     labels: xValues,
@@ -25,3 +25,27 @@ new Chart("myChart", {
     }
   }
 });
+
+new Chart("barChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      label: 'Conexiones',
+      fill: false,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    responsive: true,
+    legend: {display: true},
+    scales: {
+      xAxes: [{
+        type: 'time',
+        distribution: 'linear'
+      }]
+    }
+  }
+})
