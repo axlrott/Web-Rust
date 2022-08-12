420690a68d66b95e2d77f41c2d4c02686ee7299a
Axel Aparicio <axl_rott@hotmail.com>
2022-08-12 19:39:24 -0300

    Mejore el grafico que habia en stats para que sea en base a tiempo

--- js/code.js
+++ js/code.js
@@ -1,22 +1,27 @@
-var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+var xValues = ['2022-01-01', '2022-02-01', '2022-03-01', '2022-04-01', '2022-05-01', '2022-06-01', '2022-07-01',
+'2022-08-01', '2022-09-01', '2022-10-01', '2022-11-01', '2022-12-01'];
+var yValues = [2, 8, 10, 12, 15, 20, 22, 25, 29, 33, 40, 42];
 
 new Chart("myChart", {
   type: "line",
   data: {
     labels: xValues,
     datasets: [{
+      label: 'Conexiones',
       fill: false,
-      lineTension: 0,
       backgroundColor: "rgba(0,0,255,1.0)",
       borderColor: "rgba(0,0,255,0.1)",
       data: yValues
     }]
   },
   options: {
-    legend: {display: false},
+    responsive: true,
+    legend: {display: true},
     scales: {
-      yAxes: [{ticks: {min: 6, max:16}}],
+      xAxes: [{
+        type: 'time',
+        distribution: 'linear'
+      }]
     }
   }
 });
